81e8c3e600938c9b92dfba0c496785956110f6bf
ghanemibouchra46-star <ghanemibouchra46@gmail.com>
2026-02-16 19:39:51 +0100

    نت

--- src/pages/AdminAuth.tsx
+++ src/pages/AdminAuth.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate, useSearchParams } from 'react-router-dom';
-import { Baby, Mail, Lock, Shield, ArrowRight, User, Phone } from 'lucide-react';
+import { Baby, Mail, Lock, Shield, ArrowRight, User, Phone, Eye, EyeOff } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
