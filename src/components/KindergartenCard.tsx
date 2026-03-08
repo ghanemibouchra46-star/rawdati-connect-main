a73459b93c023362044513a3963af2d5b501817e
ghanemibouchra46-star <ghanemibouchra46@gmail.com>
2026-03-08 01:40:11 +0100

    تم

--- src/components/KindergartenCard.tsx
+++ src/components/KindergartenCard.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Kindergarten } from '@/data/kindergartens';
-import PaymentModal from '@/components/PaymentModal';
+import PaymentModal from '@/components/PaymentModalTemp';
 import { useLanguage } from '@/contexts/LanguageContext';
 import { useState } from 'react';
 
